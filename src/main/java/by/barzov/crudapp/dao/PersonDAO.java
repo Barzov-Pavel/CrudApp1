980d88d69276ebb07172d79d85af5c1029f6dda2
Barzov-Pavel <barzov_pavel@mail.ru>
2020-10-20 18:10:07 +0300

    Initial commit

---
+++ src/main/java/by/barzov/crudapp/dao/PersonDAO.java
@@ -0,0 +1,34 @@
+package by.barzov.crudapp.dao;
+
+import by.barzov.crudapp.models.Person;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private static int peopleCount;
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+        people.add(new Person(peopleCount++, "Tom"));
+        people.add(new Person(peopleCount++, "Freddy"));
+        people.add(new Person(peopleCount++, "Scarlet"));
+        people.add(new Person(peopleCount++, "Vik"));
+    }
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show(int id) {
+        for (Person person : people) {
+            if (person.getId() == id) {
+                return person;
+            }
+        }
+        return null;
+    }
+}
